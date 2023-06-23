18f167a3f6c1a0e06eab10937e0a76f1997906bc
RakeshAhire <rocky5509ahire@gmail.com>
2023-06-23 09:25:21 +0530

    cloud database add

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
 //mongoose.connect('mongodb://username:password@host:port/database?options...');
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/newdb_snaxsmart",{
+mongoose.connect("mongodb+srv://SmartVendz:Smartvendz@cluster0.wt4bcv3.mongodb.net/inventory?retryWrites=true&w=majority",{
    // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
