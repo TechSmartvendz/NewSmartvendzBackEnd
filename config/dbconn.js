46aa4311f29dbc0daf4308cfb53b69ad01940d1f
your.user.name <himanshudevgade87@gmail.com>
2023-06-30 11:50:47 +0530

    cloud mongo update

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,8 +1,8 @@
 const mongoose =require("mongoose");
 //mongoose.connect('mongodb://username:password@host:port/database?options...');
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/newdb_snaxsmart",{
-// mongoose.connect("mongodb+srv://SmartVendz:Smartvendz@cluster0.wt4bcv3.mongodb.net/inventory?retryWrites=true&w=majority",{
+// mongoose.connect("mongodb://127.0.0.1:27017/newdb_snaxsmart",{
+mongoose.connect("mongodb+srv://SmartVendz:Smartvendz@cluster0.wt4bcv3.mongodb.net/inventory?retryWrites=true&w=majority",{
    // useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
