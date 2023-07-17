8fdf1b541df93e889c0933d27b685d0645c472bd
your.user.name <himanshudevgade87@gmail.com>
2023-07-17 16:31:24 +0530

    changes updated

--- model/m_purchase_stocks.js
+++ model/m_purchase_stocks.js
@@ -3,14 +3,15 @@ const { Schema } = mongoose;
 
 const purchaseStocks = new Schema(
   {
-    warehouse: { type: Schema.Types.ObjectId, ref: "warehouse" }, 
+    warehouse: { type: Schema.Types.ObjectId, ref: "warehouse" },
     product: { type: Schema.Types.ObjectId, ref: "product" },
     supplier: { type: Schema.Types.ObjectId, ref: "supplier" },
     productQuantity: { type: Number, default: 0 },
-    sellingPrice: {type: Number, default: 0},
-    totalPrice: {type: Number, default: 0},
+    sellingPrice: { type: Number, default: 0 },
+    totalPrice: { type: Number, default: 0 },
     invoiceNumber: { type: Number, default: 0 },
     GRN_Number: { type: Number, default: 0 },
+    tax: { type: Number, default: 0 },
     admin: { type: String, default: null },
     isDeleted: { type: Boolean, default: false },
   },
