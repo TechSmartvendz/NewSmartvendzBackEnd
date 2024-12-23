1218ef428b86b52663e52699954a2d17ae2de814
Shekhar Joshi <shekhar.joshi@myparadigm.com>
2024-12-23 21:49:46 +0530

    dateformat changes

--- routes/snaxSmartAPI.js
+++ routes/snaxSmartAPI.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 const express = require('express');
 const router = express.Router();
 const axios = require('axios'); // For making requests to the external module
-const auth = require("../middleware/authentication")
+const auth = require("../middleware/authentication");
+const moment = require('moment');
+
 
 const SNAX_SMART_BASIC_AUTH = process.env.SNAX_SMART_BASIC_AUTH;
 const EXTERNAL_API_BASE_URL = process.env.EXTERNAL_API_BASE_URL;  //'https://ssmart-api-rup2dfg24a-el.a.run.app
 
 const headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Basic '+SNAX_SMART_BASIC_AUTH.trim() // Replace with your actual Base64 encoded credentials
+    'Authorization': 'Basic '+ SNAX_SMART_BASIC_AUTH // Replace with your actual Base64 encoded credentials
 };
 
 // 1. `/api/devices` - No additional parameters required
@@ -24,19 +26,18 @@ router.get('/devices', auth, async (req, res) => {
 
 // 2. `/api/txns` - Requires `fromdate` and `todate`
 router.get('/txns', auth, async (req, res) => {
-    console.log('/api/txns')
-    const { fromdate, todate } = req.query;
-
+    let { fromdate, todate } = req.query;
     if (!fromdate || !todate) {
         return res.status(400).json({ error: 'fromdate and todate are required' });
     }
 
     try {
+        fromdate=moment(fromdate).format('YYYYMMDD');
+        todate=moment(todate).format('YYYYMMDD');
         const response = await axios.get(`${EXTERNAL_API_BASE_URL}/api/txns`, {
             headers,
             params: { fromdate, todate },
         });
-        res.status(response.status).json(response.data);
     } catch (err) {
         res.status(err.response?.status || 500).json({ error: err.message });
     }
@@ -44,13 +45,10 @@ router.get('/txns', auth, async (req, res) => {
 
 // 3. `/api/txns/<deviceName>` & '/api/txns/today'- Optional `fromdate` and `todate`
 router.get('/txns/:deviceName', auth, async (req, res) => {
-    console.log('/api/txns/:deviceName')
-    const { fromdate, todate } = req.query;
+    let { fromdate, todate } = req.query;
     const { deviceName } = req.params;
-    console.log(deviceName.toUpperCase)
     if(deviceName.toUpperCase() === "TODAY"){
         try {
-            console.log('/api/txns/today')
             const response = await axios.get(`${EXTERNAL_API_BASE_URL}/api/txns/today`, {headers});
             res.status(response.status).json(response.data);
         } catch (err) {
@@ -63,6 +61,8 @@ router.get('/txns/:deviceName', auth, async (req, res) => {
         }
     
         try {
+            fromdate=moment(fromdate).format('YYYYMMDD');
+            todate=moment(todate).format('YYYYMMDD');
             const response = await axios.get(`${EXTERNAL_API_BASE_URL}/api/txns/${deviceName}`, {
                 headers,
                 params: { fromdate, todate },
